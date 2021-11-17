8b9867b411bd28caaa8a83e2880a155970c88334
Karim-Valeev <karim.valeev.i@gmail.com>
2021-11-17 12:02:29 +0300

    Add whitenoise

--- src/django_notion/settings.py
+++ src/django_notion/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -193,6 +194,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/uploaded_files/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "src", "uploaded_files")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
